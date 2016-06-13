e08d2b783eae11cd1949bd4c4aa90e45a70ea743
FWYT <francest1995@hotmail.com>
2016-06-13 15:44:07 +0000

    Allow user to specify comparison operator

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,7 @@
+import com.mysema.commons.lang.Pair;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
-import com.querydsl.core.types.Path;
-import com.querydsl.core.types.PathImpl;
-import com.querydsl.core.types.Visitor;
+import com.querydsl.core.types.*;
 import com.querydsl.core.types.dsl.*;
 import com.querydsl.sql.*;
 import com.querydsl.sql.types.LocalDateTimeType;
@@ -40,7 +39,7 @@ public class main {
         SQLQueryFactory qF = new SQLQueryFactory(config, dataSource);
 
         PathBuilder<Object> playPath = new PathBuilder<Object>(Object.class, "playground");
-        StringPath typePath = Expressions.stringPath(playPath, "type");
+        Path<String> typePath = Expressions.path(String.class, playPath, "type");
         StringPath locationPath = Expressions.stringPath(playPath, "location");
         StringPath colorPath = Expressions.stringPath(playPath, "color");
         NumberPath<Integer> idPath = Expressions.numberPath(Integer.class, playPath, "id");
@@ -54,14 +53,19 @@ public class main {
 
 
         BooleanBuilder where = new BooleanBuilder();
-        HashMap<String,String> filters = new HashMap<String,String>();
-        filters.put("type", "slide");
-        filters.put("location", "east");
-        filters.put("color", "yellow");
+        HashMap<String,Pair<String,String>> filters = new HashMap<String,Pair<String,String>>();
+        filters.put("type", new Pair("slide", "="));
+        filters.put("location", new Pair("east", "="));
+        filters.put("color", new Pair("yellow", "="));
+        filters.put("install_date", new Pair("2009-02-09", ">"));
 
-        where.and(typePath.eq(filters.get("type")));
+        Operator op = Ops.LOE;
+        Predicate p = Expressions.predicate(op, timePath, Expressions.constant(filters.get("install_date").getFirst()));
+
+        //where.and(typePath.);
         //where.and(colorPath.eq(filters.get("color")));
         //where.and(locationPath.eq(filters.get("location")));
+        where.and(p);
 
         BooleanBuilder sort = new BooleanBuilder();
         HashMap<String, String> sorts = new HashMap<String, String>();
@@ -70,7 +74,7 @@ public class main {
         sorts.put("color", "desc");
         sorts.put("date", "asc");
 
-        SQLQuery b = qF.select(all).from(playPath).where(dotsPath.eq(3.5)).limit(10);
+        SQLQuery b = qF.select(all).from(playPath).where(where).orderBy(datePath.asc());
         //b.orderBy(locationPath.asc());
         //b.orderBy(colorPath.asc());
 
@@ -82,7 +86,7 @@ public class main {
         //List<Tuple> res = qF.select(idPath, locationPath, colorPath, typePath, datePath).from(playPath).where(timePath.stringValue().eq("2016-06-03 18:22:45.64+00")).fetch();
 
 
-        //SQLBindings bindings = qF.select(all).from(playPath).where(timePath.stringValue().eq("2016-06-03 18:22:45.64+00"))
+        //SQLBindings bindings = qF.select(all).from(playPath).where(where).getSQL();
                 //.orderBy(locationPath.asc()).limit(10).getSQL();
 
         //System.out.println(bindings.getSQL()+"\n");
@@ -96,48 +100,84 @@ public class main {
         return "Results done!";
     }
 
-    static BooleanBuilder returnFilterBooleanBuilder(BooleanBuilder where, String key, int tp, String value, PathBuilder playPath)
+    static Operator getOp(String cmp) //note: key is to the left and value is to the right. So key <cmp> value.
     {
+        if (cmp == "=")
+        {
+            return Ops.EQ;
+        }
+        else if (cmp == "!=")
+        {
+            return Ops.NE;
+        }
+        else if (cmp == ">")
+        {
+            return Ops.GT;
+        }
+        else if (cmp == ">=")
+        {
+            return Ops.GOE;
+        }
+        else if (cmp == "<")
+        {
+            return Ops.LT;
+        }
+        else if (cmp == "<=")
+        {
+            return Ops.LOE;
+        }
+        else
+        {
+            System.out.println("Invalid comparison operator.");
+            System.exit(1);
+            return null;
+        }
+    }
+
+    static BooleanBuilder returnFilterBooleanBuilder(BooleanBuilder where, String key, int tp, String value, String cmp, PathBuilder playPath)
+    {
+        Operator op = getOp(cmp);
+        Predicate p;
         switch (tp)
         {
             case 12: //varchar
                 StringPath sp = Expressions.stringPath(playPath, key);
-                where.and(sp.eq(value));
-                return where;
+                p = Expressions.predicate(op, sp, Expressions.constant(value));
+                return where.and(p);
             case 8: //double
                 NumberPath<Double> fp = Expressions.numberPath(Double.class, playPath, key);
-                where.and(fp.eq(Double.parseDouble(value)));
-                return where;
+                p = Expressions.predicate(op, fp, Expressions.constant(value));
+                return where.and(p);
             case 4: //integer
                 NumberPath<Integer> np = Expressions.numberPath(Integer.class, playPath, key);
-                where.and(np.eq(Integer.parseInt(value)));
-                return where;
+                p = Expressions.predicate(op, np, Expressions.constant(value));
+                return where.and(p);
             case 2013: //Time with timezone
             case 2014: //timestamp with time zone
             case 93: //timestamp
                 DateTimePath<Timestamp> dtp = Expressions.dateTimePath(Timestamp.class, playPath, key);
-                where.and(dtp.stringValue().eq(value));
-                return where;
+                p = Expressions.predicate(op, dtp, Expressions.constant(value));
+                return where.and(p);
             case 91: //date
                 DatePath<LocalDate> dp = Expressions.datePath(LocalDate.class, playPath, key);
-                where.and(dp.stringValue().eq(value));
-                return where;
+                p = Expressions.predicate(op, dp, Expressions.constant(value));
+                return where.and(p);
             case 16: //boolean
             case -7: //boolean BIT
                 BooleanPath tfPath = Expressions.booleanPath(playPath, key);
                 if (value == "true")
                 {
-                    where.and(tfPath.eq(true));
+                    p = Expressions.predicate(op, tfPath, Expressions.constant(true));
                 }
                 else
                 {
-                    where.and(tfPath.eq(false));
+                    p = Expressions.predicate(op, tfPath, Expressions.constant(false));
                 }
-                return where;
+                return where.and(p);
             case 1111: //other, aka UUID
-                StringPath op = Expressions.stringPath(playPath, key);
-                where.and(op.eq(value));
-                return where;
+                StringPath otp = Expressions.stringPath(playPath, key);
+                p = Expressions.predicate(op, otp, Expressions.constant(value));
+                return where.and(p);
         }
         return null;
     }
@@ -252,14 +292,12 @@ public class main {
                     System.out.println("Not valid double.");
                     return false;
                 }
-                System.out.println("valid");
                 return true;
             case 4: //integer
                 if (!value.matches("^[-]?[0-9]+$")) {
                     System.out.println("Not valid integer");
                     return false;
                 }
-                System.out.println("valid");
                 return true;
             case 2013: //Time with timezone
                 if (!value.matches("^" + time+"(.*)?"))
@@ -267,16 +305,14 @@ public class main {
                     System.out.println("Not valid timestamp.");
                     return false;
                 }
-                System.out.println("valid");
                 return true;
 
             case 93: //timestamp
-                if (!value.matches("^"+mm+c+dd+c+yyyy + "((\\s)" + time +")?") && !value.matches("^"+yyyy+c+mm+c+dd + "((\\s)" + time +")?"))
+                if (!value.matches("^"+mm+c+dd+c+yyyy + "((\\s)" + time +")?(.*)") && !value.matches("^"+yyyy+c+mm+c+dd + "((\\s)" + time +")?(.*)"))
                 {
                     System.out.println("Not valid timestamp.");
                     return false;
                 }
-                System.out.println("valid");
                 return true;
             case 2014: //timestamp with time zone //must have date
                 if (!value.matches("^"+mm+c+dd+c+yyyy + "((\\s)" + time +")?(.*)") && !value.matches("^"+yyyy+c+mm+c+dd + "((\\s)" + time +")?(.*)"))
@@ -284,7 +320,6 @@ public class main {
                     System.out.println("Not valid timestamp.");
                     return false;
                 }
-                System.out.println("valid");
                 return true;
             case 91: //date
                 //mm/dd/yyyy, first number always becomes month
@@ -293,7 +328,6 @@ public class main {
                     System.out.println("Not valid date. Make sure numbers are within appropriate bounds. Valid format: mm/dd/yyyy or mm-dd-yyyy or yyyy-mm-dd or yyyy/mm/dd.");
                     return false;
                 }
-                System.out.println("valid");
                 return true;
             case 16: //boolean
             case -7: //boolean BIT
@@ -338,7 +372,7 @@ public class main {
         return true;
     }
 
-    static void queryGeneric(HashMap<String,String> filter, LinkedHashMap<String,Integer> sort, int limit, PGPoolingDataSource dataSource, Connection c)
+    static void queryGeneric(HashMap<String,Pair<String, String>> filter, LinkedHashMap<String,Integer> sort, int limit, PGPoolingDataSource dataSource, Connection c)
     {
         System.out.println("Start generic query \n");
         SQLTemplates templates = new PostgreSQLTemplates().builder().printSchema().build();
@@ -348,15 +382,16 @@ public class main {
         SQLQueryFactory qF = new SQLQueryFactory(config, dataSource);
 
         //path to the table
-        PathBuilder<Object> playPath = new PathBuilder<Object>(Object.class, "schema_playground.schemaplay");
+        PathBuilder<Object> playPath = new PathBuilder<Object>(Object.class, "playground");
 
         Iterator filterI = filter.entrySet().iterator();
         BooleanBuilder where = new BooleanBuilder();
 
-        for (Map.Entry<String, String> entry : filter.entrySet())
+        for (Map.Entry<String, Pair<String, String>> entry : filter.entrySet())
         {
             String key = entry.getKey(); //column name
-            String value = entry.getValue(); //what it should be equal to
+            String value = entry.getValue().getFirst(); //what it should be equal to
+            String cmp = entry.getValue().getSecond(); //the comparison operator:
 
             //check that column is valid
             if (!validColumn(key))
@@ -365,7 +400,7 @@ public class main {
                 System.exit(1);
             }
 
-            //System.out.println(key + " " + value +"\n");
+            System.out.println(key + " " + value + " " + cmp + "\n");
             int tp = colTypes.get(key);
 
             if (!validValue(value, tp))
@@ -373,7 +408,7 @@ public class main {
                 System.out.println("Invalid value. Make sure filter values are of correct syntax.");
                 System.exit(1);
             }
-            where = returnFilterBooleanBuilder(where, key, tp, value, playPath);
+            where = returnFilterBooleanBuilder(where, key, tp, value, cmp,playPath);
         }
 
         SQLQuery sql = qF.select(all).from(playPath).where(where).limit(limit);
@@ -433,7 +468,7 @@ public class main {
                 String u = uuid.toString();
                 String com = UUID.randomUUID().toString();
                 int tf = rand.nextInt(2);
-                sql = "INSERT INTO schema_playground.schemaplay(type,location,color,install_date, uuid, comment, trueFalse) VALUES ('" + type.get(n) + "','" + location.get(o) +
+                sql = "INSERT INTO playground (type,location,color,install_date, uuid, comment, trueFalse) VALUES ('" + type.get(n) + "','" + location.get(o) +
                         "','" + color.get(m) + "','" + month + "-" + day + "-" + year + "','" + u + "','" + com + "','" +
                         b.get(tf) + "');";
                 stmt.executeUpdate(sql);
@@ -455,6 +490,7 @@ public class main {
             Statement stmt = null;
             stmt = c.createStatement();
             ResultSet sql = stmt.executeQuery("SELECT * FROM playground");
+            //ResultSet sql = stmt.executeQuery("select * from playground where cast(playground.install_date as varchar) >= '2015-02-09' order by playground.install_date asc");
 
             while (sql.next()) {
                 int id = sql.getInt("id");
@@ -534,15 +570,15 @@ public class main {
             //don't need number to sql map for actual usability
             generateMaps(c);
 
-            HashMap<String,String> filters = new HashMap<String,String>();
+            HashMap<String,Pair<String,String>> filters = new HashMap<String,Pair<String,String>>();
             //filters.put("type", "swing");
-            filters.put("location", "east");
+            //filters.put("location", "east");
             //filters.put("color", "yellow");
-            filters.put("install_date", "1912-05-02");
-            //filters.put("time_stamp","2016-06-07 16:47:58.642+00");
+            //filters.put("install_date", new Pair("2015-5-2",">"));
+            filters.put("time_stamp", new Pair("2016-06-07 16:47:58.642+00", ">="));
             //filters.put("uuid","bd3a1773-e047-4394-a4b8-c984f0232410");
-            //filters.put("truefalse", "false");
-            //filters.put("dots", "3.5");
+            //filters.put("truefalse", new Pair("false", "="));
+            filters.put("dots", new Pair("3.5", "="));
             //filters.put("lines", "4");
 
             //System.out.println(colTypes.get("trueFalse"));
@@ -559,7 +595,9 @@ public class main {
             //query(dataSource, c);
             queryGeneric(filters, sorts, 10, dataSource, c);
             //insertRandom(c);
+            //printAll(c);
 
+            //System.out.println(getOp("p"));
 
             c.close();
         } catch (Exception e)
